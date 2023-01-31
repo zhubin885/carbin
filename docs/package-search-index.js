62dd2f79dd76513514bec3061709f44d620a0312
zhubin <zhubin@coocaa.com>
2023-01-31 16:12:29 +0800

    Add docs api

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"android.car.drivingstate","h":"","u":""},{"l":"所有程序包","h":"","u":"allpackages-index.html"}]
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"CarDrivingAssistanceManager","h":"","u":""},{"l":"CarDrivingAssistanceManager.CarDrivingEventCallback","h":"","u":""},{"l":"CarDrivingAssistanceManager.PropertyId","h":"","u":""},{"l":"所有类","h":"","u":"allclasses-index.html"}]
